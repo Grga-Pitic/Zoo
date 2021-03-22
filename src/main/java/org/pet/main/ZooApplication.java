dcb85742bb76ed01ebcaf7d8a29dbbe4ee2b9b3b
Ivan <ikon1998@mail.ru>
2021-03-22 17:16:33 +0300

    Creates web app

---
+++ src/main/java/org/pet/main/ZooApplication.java
@@ -0,0 +1,11 @@
+package org.pet.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZooApplication.class, args);
+    }
+}
